c7eb549fe797ecdad0017a38a825f4b6eee6761d
Gerardo Belot <gerardo.belot@gmail.com>
2019-11-22 10:38:32 -0600

    WIP: Agregando validacion dependiento de que componente se active

--- src/app/autenticacion/register/administrador-sistema/administrador-sistema.component.ts
+++ src/app/autenticacion/register/administrador-sistema/administrador-sistema.component.ts
@@ -1,16 +1,22 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {Component, Input, OnInit, OnDestroy} from '@angular/core';
+import {FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-administrador-sistema',
   templateUrl: './administrador-sistema.component.html',
   styleUrls: ['./administrador-sistema.component.sass']
 })
-export class AdministradorSistemaComponent implements OnInit {
+export class AdministradorSistemaComponent implements OnInit, OnDestroy{
   @Input() ParentForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.ParentForm.controls.contrato.validator = Validators.required;
+    this.ParentForm.controls.fvencimiento.validator = Validators.required;
   }
 
+  ngOnDestroy(){
+    this.ParentForm.controls.contrato.validator = null;
+    this.ParentForm.controls.fvencimiento.validator = null;
+  }
 }
--- src/app/autenticacion/register/verificador/verificador.component.ts
+++ src/app/autenticacion/register/verificador/verificador.component.ts
@@ -1,16 +1,23 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {Component, Input, OnInit, OnDestroy} from '@angular/core';
+import {FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-verificador',
   templateUrl: './verificador.component.html',
   styleUrls: ['./verificador.component.sass']
 })
-export class VerificadorComponent implements OnInit {
+export class VerificadorComponent implements OnInit, OnDestroy {
   @Input() ParentForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.ParentForm.controls.contrato.validator = Validators.required;
+    this.ParentForm.controls.fvencimiento.validator = Validators.required;
+  }
+
+  ngOnDestroy(){
+    this.ParentForm.controls.contrato.validator = null;
+    this.ParentForm.controls.fvencimiento.validator = null;
   }
 
 }
