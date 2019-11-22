e609815fb0f0cbea8c8342e507571137c4784ce2
Gerardo Belot <gerardo.belot@gmail.com>
2019-11-22 10:32:07 -0600

    WIP: Agregando validacion dependiento de que componente se active

--- src/app/autenticacion/register/gerente/gerente.component.ts
+++ src/app/autenticacion/register/gerente/gerente.component.ts
@@ -1,16 +1,25 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {Component, Input, OnInit, OnDestroy} from '@angular/core';
+import {FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-gerente',
   templateUrl: './gerente.component.html',
   styleUrls: ['./gerente.component.sass']
 })
-export class GerenteComponent implements OnInit {
+export class GerenteComponent implements OnInit, OnDestroy {
   @Input() ParentForm: FormGroup;
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.ParentForm.controls.contrato.validator = Validators.required;
+    this.ParentForm.controls.fautorizacion.validator = Validators.required;
+
+  }
+
+  ngOnDestroy(){
+    this.ParentForm.controls.contrato.validator = null;
+    this.ParentForm.controls.fautorizacion.validator = null;
+  }
 }
