3627f93c6cbbaa3847d82914185836d3d6dc1adf
Gerardo Belot <gerardo.belot@gmail.com>
2019-11-22 12:24:55 -0600

    WIP: fix de validacion

--- src/app/autenticacion/register/productor-representante/productor-representante.component.ts
+++ src/app/autenticacion/register/productor-representante/productor-representante.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit, OnDestroy} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {FormGroup, Validators} from "@angular/forms";
 import {GruposServiceService} from "../../../_servicios/grupos-service.service";
 
 @Component({
@@ -17,10 +17,10 @@ export class ProductorRepresentanteComponent implements OnInit, OnDestroy{
     this.grupoService.handler().subscribe(data => {
       this.grupos = data;
     });
-    this.ParentForm.controls.identidad.validator = null;
-    this.ParentForm.controls.rtn.validator = null;
-    this.ParentForm.controls.rtn_image.validator = null;
-    this.ParentForm.controls.grupo_id.validator = null;
+    this.ParentForm.controls.identidad.validator = Validators.required;
+    this.ParentForm.controls.rtn.validator = Validators.required;
+    this.ParentForm.controls.rtn_image.validator = Validators.required;
+    this.ParentForm.controls.grupo_id.validator = Validators.required;
   }
 
   ngOnDestroy() {
--- src/app/autenticacion/register/transportista/transportista.component.ts
+++ src/app/autenticacion/register/transportista/transportista.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-transportista',
@@ -11,11 +11,11 @@ export class TransportistaComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    this.ParentForm.controls.identidad.validator = null;
-    this.ParentForm.controls.rtn.validator = null;
-    this.ParentForm.controls.rtn_image.validator = null;
-    this.ParentForm.controls.cuenta_image.validator = null;
-    this.ParentForm.controls.descripcion_vehiculos.validator = null;
+    this.ParentForm.controls.identidad.validator = Validators.required;
+    this.ParentForm.controls.rtn.validator = Validators.required;
+    this.ParentForm.controls.rtn_image.validator = Validators.required;
+    this.ParentForm.controls.cuenta_image.validator = Validators.required;
+    this.ParentForm.controls.descripcion_vehiculos.validator = Validators.required;
   }
 
   ngOnDestroy() {
